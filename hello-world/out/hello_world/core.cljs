419d189f573593985dd12d1a5f29a2b3e65b1e82
gjeanmart <gregoire.jeanmart@gmail.com>
2020-03-09 14:22:43 +0000

    delete non src files

--- hello-world/out/hello_world/core.cljs
+++
@@ -1,6 +0,0 @@
-(ns hello-world.core
-  (:require react-dom))
-
-(.render js/ReactDOM
-  (.createElement js/React "h2" nil "Hello, React2")
-  (.getElementById js/document "app"))
--- hello-world/out/hello_world/core.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.10.520 {}
-goog.provide('hello_world.core');
-goog.require('cljs.core');
-goog.require('react_dom');
-hello_world.core.global$module$react_dom = goog.global["ReactDOM"];
-ReactDOM.render(React.createElement("h2",null,"Hello, React2"),document.getElementById("app"));
-
-//# sourceMappingURL=core.js.map
